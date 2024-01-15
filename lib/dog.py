e68b38cf01ee13081d5aff76eb30019da8e3b6d9
denis <denniiskim@gmail.com>
2024-01-15 04:10:07 +0300

    completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -5,3 +5,11 @@ class Dog:
 
     #Instance method definition
     pass
+    def bark(self):
+        print("Woof!")
+    
+    def sit(self):
+        print("The dog is sitting.")
+
+# fido = Dog()
+# fido.bark()
--- lib/person.py
+++ lib/person.py
@@ -5,3 +5,8 @@ class Person:
 
     #Instance method definition
     pass
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
